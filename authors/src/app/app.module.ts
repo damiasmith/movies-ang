ce69362cebb83c727fc69174ba7b5d8db389b77d
Damia Smith <Damia@Damia-Smiths-MacBook-Pro.local>
2019-04-01 22:46:29 -0500

    assignment1

---
+++ authors/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AuthorComponent } from './author/author.component';
+import { AuthorService } from './author.service';
+import { FavoriteComponent } from './favorite/favorite.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthorComponent,
+    FavoriteComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [AuthorService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
